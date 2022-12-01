daf5a5f934e143d9a97cd3f47827619a164bb940
James Fincher <heartandkarma@gmail.com>
2022-12-01 10:03:30 -0800

    setup to work with aws amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "circuitc7270a7f": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "function": {
+        "S3Triggere644a2c6": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "s305015001": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
